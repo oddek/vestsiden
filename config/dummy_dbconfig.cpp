b6636bc77315e903e150e8dc6d8b055ccf737151
Kent Odde <kentodde89@gmail.com>
2020-11-14 18:55:12 +0100

    Added dummy config files

---
+++ config/dummy_dbconfig.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+const std::string sourceDbHostNameV6 = "[0000:000:0000:00::00]";
+const std::string sourceDbPort = "0000";
+const std::string sourceDbUsername = "";
+const std::string sourceDbPassword = "";
+const std::string sourceDbScheme = "";
+
+
+const std::string destDbHostNameV6 = "";
+const std::string destDbPort = "";
+const std::string destDbUsername = "";
+const std::string destDbPassword = "";
+const std::string destDbScheme = "";
+
