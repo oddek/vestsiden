d18b86bce7090ed5537d13c3d186bc03c815deca
Kent Odde <kentodde89@gmail.com>
2020-11-19 18:51:05 +0100

    Removed log cat in periodic filler cron script

--- bash/periodicDbInsert.sh
+++ bash/periodicDbInsert.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+
+# logFile='/home/kent/source/work/vestsiden/src/periodicInsert/status.log'
+
 echo 'Starting periodicFiller'
-../src/periodicInsert/fill
-echo 'Done'
+# ../src/periodicInsert/fill
+
+#Print log to terminal, so that it will be mailed by cronjob
+#Needs to set up MTA to get this working, not sure if this is necessary
+# logContent=$(cat "$logFile")
+# echo "$logContent"
